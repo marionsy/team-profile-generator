56b136e116cad145674386ef1234af85af8703be
marionsy <marionantoniette.sy@gmail.com>
2022-08-23 17:32:51 -0700

    created manager test and class function

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -5,5 +5,17 @@ describe("Engineer", () => {
         const testValue = "GitHub user";
         const e = new Engineer("name", 1, "test@test.com", testValue);
         expect(e.github).toEqual(testValue);
-    })
+    });
+
+    it("should return GitHub username", () => {
+        const testValue =  "GitHub user";
+        const e = new Engineer("name", 1, "test@test.com", testValue);
+        expect(e.getGithub()).toEqual(testValue);
+    });
+
+    it("should return Engineer", () => {
+        const testValue = "Engineer"
+        const e = new Engineer("name", 1, "test@test.com", "GitHub user");
+        expect(e.getRole()).toEqual(testValue);
+    });
 })
---
+++ tests/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    it("should set office number", () => {
+        const testValue = 1;
+        const m = new Manager("name", 1, "test@test.com", testValue);
+        expect(m.officeNumber).toEqual(testValue);
+    });
+
+    it("should return office number", () => {
+        const testValue = 1;
+        const m = new Manager("name", 1, "test@test.com", testValue);
+        expect(m.getOfficeNumber()).toEqual(testValue);
+    });
+
+    it("should return Manager", () => {
+        const testValue = "Manager"
+        const m = new Manager("name", 1, "test@test.com", 1);
+        expect(m.getRole()).toEqual(testValue);
+    });
+})
